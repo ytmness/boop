13c259c672a5d020c665be19f4265a0ca78b4827
ytmness <sergiooresa@gmail.com>
2025-12-14 16:25:31 -0600

    Subir aÃºn mÃ¡s el logo BOOP y burbujas eliminando padding extra

--- boop-ios/BoopApp/BoopApp/Views/Events/EventsHubView.swift
+++ boop-ios/BoopApp/BoopApp/Views/Events/EventsHubView.swift
@@ -159,7 +159,7 @@ private struct HomeOverlayHeader: View {
                     }
                 }
             }
-            .padding(.top, geo.safeAreaInsets.top + 2)
+            .padding(.top, geo.safeAreaInsets.top)
             .padding(.bottom, 10)
             .frame(maxWidth: .infinity, alignment: .top)
         }
